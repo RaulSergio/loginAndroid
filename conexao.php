06916615b66411a3e12fd92cbd00d2cd9504b4bd
RaulSergio <dragolaymann@gmail.com>
2017-08-20 14:52:57 -0400

    colocando coluna confirmaSenha

--- conexao.php
+++ conexao.php
@@ -8,9 +8,9 @@ $dbcon = new MySQLi("$host","$usuario","$senha","$banco");
 
 if($dbcon->connect_error){
     echo "Erro de Conexão";
-}/*else{
+}else{
     echo "Conexão OK";
-}*/
+}
 
 mysqli_query($dbcon,"SET NAMES UTF8");
 ?>
--- registrar.php
+++ registrar.php
@@ -5,6 +5,7 @@ include_once 'conexao.php';
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
+$confirmaSenha = $_POST['confirmaSenha'];
 
 $sqlValida = $dbcon->query("SELECT * FROM tblogin WHERE email='$email'");
 
@@ -12,7 +13,7 @@ if(mysqli_num_rows($sqlValida)>0){
     echo "Erro no Email";
 }else{
     //echo "deu certo";
-    $sqlRegistra = $dbcon->query("INSERT INTO tblogin(nome, email, senha) VALUES('$nome','$email','$senha')");
+    $sqlRegistra = $dbcon->query("INSERT INTO tblogin(nome, email, senha, confirmaSenha) VALUES('$nome','$email','$senha','$confirmaSenha')");
 
     if($sqlRegistra){
         echo "Usuario Registrado";
